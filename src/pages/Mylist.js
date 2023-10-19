745fbcbbe8f3fc93c3b1fabdc8a5899c8aea5b46
Leonardosagica <126108886+Leonardosagica@users.noreply.github.com>
2023-10-19 14:20:12 +0100

    Add files via upload

---
+++ src/pages/Mylist.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Mylist.css';
+
+function Mylist() {
+	return (
+		<div className='mylist__nav'>
+			{/* <div className='mylist__nav__contents'>
+				<div className='logo__wrapper'>
+					<img className='nav__logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Logonetflix.png/800px-Logonetflix.png?20170904093427' alt='' />
+				</div>
+				<div className='mylist__theme'>
+					<h1>My List</h1>
+				</div>
+				</div>
+
+				<div className='mylist__avatar__wrapper'>
+					<img className='nav__avatar' src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png' alt='' /> </div>*/}
+
+			<h2>its my list page</h2>
+		</div>
+	);
+}
+
+export default Mylist;
