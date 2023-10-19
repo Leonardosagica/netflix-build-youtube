7567512b17501a6eb018895ffdf39d943b71efd5
Leonardosagica <126108886+Leonardosagica@users.noreply.github.com>
2023-10-19 16:48:44 +0100

    Delete src directory

--- src/server/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-	apiKey: 'AIzaSyCZyYmr7XaM-m38UoHry3IBLggUUAKzmj8',
-	authDomain: 'netflix-clone-fc210.firebaseapp.com',
-	projectId: 'netflix-clone-fc210',
-	storageBucket: 'netflix-clone-fc210.appspot.com',
-	messagingSenderId: '648727032463',
-	appId: '1:648727032463:web:12266b91733558632836fa',
-};
